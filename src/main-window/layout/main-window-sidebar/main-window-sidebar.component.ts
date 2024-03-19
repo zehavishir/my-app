e4793872f1b5e569d63f8e664fca8bc40283d6e3
Shir Zehavi <shirzehavi13@gmail.com>
2024-03-19 09:36:08 +0200

    added sidebar when you click on category and it filters the blocks

--- src/main-window/layout/main-window-sidebar/main-window-sidebar.component.ts
+++ src/main-window/layout/main-window-sidebar/main-window-sidebar.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Job } from 'src/interfaces/job.interface';
 
 @Component({
   selector: 'app-main-window-sidebar',
   templateUrl: './main-window-sidebar.component.html',
   styleUrls: ['./main-window-sidebar.component.scss']
 })
-export class MainWindowSidebarComponent {
+export class MainWindowSidebarComponent implements OnInit{
+  public Sales: number;
+  public MarketingAndSales: number;
+  public Seo: number;
+  public CopywritingAndContent: number;
+  @Input() set JobsToDisplay(jobs: Job[]){
+    this.Sales = jobs.filter((x: Job) => x.jobIndustry[0] === 'Sales').length;
+    this.MarketingAndSales = jobs.filter((x: Job) => x.jobIndustry[0] === 'Marketing &amp; Sales').length;
+    this.Seo = jobs.filter((x: Job) => x.jobIndustry[0] === 'SEO').length;
+    this.CopywritingAndContent = jobs.filter((x: Job) => x.jobIndustry[0] === 'Copywriting &amp; Content').length; 
+    console.log(this.Sales);
+    console.log(this.MarketingAndSales);
+    console.log(this.Seo);
+    console.log(this.CopywritingAndContent);
+  }
+  @Output() OnClick = new EventEmitter<string>;
+  constructor(){
+    
+  }
+  ngOnInit(): void {
+    // this.Sales = this.JobsToDisplay.filter((x: Job) => x.jobIndustry[0] === 'Sales').length;
+    // console.log(this.Sales)
+  }
 
+  public OnClickTab(str: string): void{
+    this.OnClick.emit(str)
+  }
 }
--- src/main-window/main-window.component.ts
+++ src/main-window/main-window.component.ts
@@ -9,14 +9,36 @@ import { Data } from 'src/interfaces/data.interface';
 })
 export class MainWindowComponent implements OnInit {
   public JobsArray: Job[];
+  public JobsToDisplay: Job[];
   constructor(private http: HttpClient){
     this.JobsArray = [];
+    this.JobsToDisplay = this.JobsArray
   }
   ngOnInit(): void {
     this.http.get<Data>('https://jobicy.com/api/v2/remote-jobs?count=20&geo=usa&industry=marketing&tag=seo').subscribe((response : Data) => {
       // console.log(response.jobs.map((x: Job) => x.salaryCurrency));
       console.log(response.jobs);
       this.JobsArray = response.jobs;
+      this.JobsToDisplay = this.JobsArray
     });
+  }
+
+  public OnClick(str: string): Job[]{
+    switch (str) {
+      case 'Marketing & Sales':
+        this.JobsToDisplay = this.JobsArray.filter((x: Job) => x.jobIndustry[0] === 'Marketing &amp; Sales');
+        return  this.JobsToDisplay;
+        case 'Sales':
+          this.JobsToDisplay = this.JobsArray.filter((x: Job) => x.jobIndustry[0] === 'Sales');
+          return  this.JobsToDisplay;
+        case 'SEO':
+          this.JobsToDisplay = this.JobsArray.filter((x: Job) => x.jobIndustry[0] === 'SEO');
+          return  this.JobsToDisplay;
+        case 'Copywriting & Content':
+          this.JobsToDisplay = this.JobsArray.filter((x: Job) => x.jobIndustry[0] === 'Copywriting &amp; Content');
+          return  this.JobsToDisplay;
+      default:
+        return this.JobsArray;
+    }
   }
 }
